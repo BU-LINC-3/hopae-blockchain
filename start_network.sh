0714a3f9d9b41f5bc12a1a305a8523359a7916e5
paragonnov <qkdxorjs1002@naver.com>
2021-10-20 20:56:55 +0900

    SUB

--- start_network.sh
+++ start_network.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 export PATH=$PATH:~/.local/bin
 
+## PATCH?????????????????????????
+cp -f ./dists/von/Dockerfile ./von/Dockerfile
+cp -f ./dists/tails/Dockerfile.test ./tails/docker/Dockerfile.test
+cp -f ./dists/von/Dockerfile.tails-server ./tails/docker/Dockerfile.tails-server
+
 ./von/manage build
 ./von/manage start
 
